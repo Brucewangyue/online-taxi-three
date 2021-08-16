b96fafa19b3a69ca5e1bb969db6cbd11083aa61d
王越 <yue.wang@szctjt.cn>
2021-08-16 16:12:30 +0800

    服务监控，调用链跟踪

---
+++ cloud-admin/src/main/java/com/van/cloudadmin/CloudAdminApplication.java
@@ -0,0 +1,13 @@
+package com.van.cloudadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminApplication.class, args);
+    }
+
+}
